3c7fcca492a52bebf73e5fc1774a18a7010d409b
Mikoee <3432010031@qq.com>
2025-10-03 23:59:38 +0800

    asd

--- webpack-studay/demo/src/app.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: xyyvscode 3432010031@qq.com
- * @Date: 2023-12-08 16:58:25
- * @LastEditors: xyyvscode 3432010031@qq.com
- * @LastEditTime: 2023-12-08 16:58:38
- * @FilePath: /vscode document/webpack/demo/src/app.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-console.log('appaaaaaaaa');
--- webpack-studay/src/dist/static/js/main.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";console.log(2-1-2),console.log(function(...o){return o.reduce(((o,n)=>o+n),0)}(1,2))}();
-//# sourceMappingURL=main.js.map
